f8f5d7207453d2ac8a03a2e9633123a0de2453e4
a.konovalov <a.konovalov@abbyy-ls.com>
2014-04-11 10:43:16 +0400

    cosm(grunt): add util

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,29 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         uglify: {
             dest: {
                 options: {
                     sourceMap: true,
-                    sourceMapName: 'dest/angular-fsm.min.map'
+                    sourceMapName: 'dist/angular-fsm.min.map'
                 },
                 files: {
-                    'dest/angular-fsm.min.js': ['src/fsm.js']
+                    'dist/angular-fsm.min.js': ['src/fsm.js']
                 }
             }
         },
         bump: {
-
+            options: {
+                updateConfigs: ['pkg','bower.json'],
+                commitFiles: ['package.json','bower.json','CHANGELOG.md'],
+                commitMessage: 'chore: release v%VERSION%',
+                pushTo: 'upstream'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-conventional-changelog');
 }
